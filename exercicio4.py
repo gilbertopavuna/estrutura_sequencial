b7cb57b6be3c96e795d319fd95af7509df6bfb09
Gilberto Pavuna <gilbertopavunacosta@hotmail.com>
2022-12-14 09:25:55 -0300

    Exercicio 4 realizado

---
+++ exercicio4.py
@@ -0,0 +1,7 @@
+# Faça um Programa que peça as 4 notas bimestrais e mostre a média.
+nota1 = float(input('Entre com a primeira nota bimestral: '))
+nota2 = float(input('Entre com a segunda nota bimestral: '))
+nota3 = float(input('Entre com a terceira nota bimestral: '))
+nota4 = float(input('Entre com a quarta nota bimestral: '))
+media = (nota1 + nota2 + nota3 + nota4)/4
+print(f'A sua média foi de : {media}')
