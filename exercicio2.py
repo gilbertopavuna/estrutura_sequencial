9c3770bbf3e82ee59c776abb0d11bcce408738f9
Gilberto Pavuna <gilbertopavunacosta@hotmail.com>
2022-12-14 08:52:51 -0300

    resposta pessoal do exercicio 2 python brasil Estruturas Sequenciais

---
+++ exercicio2.py
@@ -0,0 +1,3 @@
+#Faça um Programa que peça um número e então mostre a mensagem O número informado foi [número].
+numero = int(input('Entre com um número inteiro: '))
+print(f'O número informado é {numero}')
