bc6b41bfff3f0876894f85c7b82ef7154ff719aa
Gilberto Pavuna <gilbertopavunacosta@hotmail.com>
2022-12-14 20:54:29 -0300

    Exercicio 11 resolvido

---
+++ exercicio11.py
@@ -0,0 +1,9 @@
+n1 = float(input('Entre com um número real: '))
+n2 = int(input('Entre com um número inteiro: '))
+n3 = int(input('Entre com um número inteiro: '))
+produto = (n1*2) * (n2/2)
+soma = (n1*3) + n3
+elevado = n3**3
+print(f'O produto do dobro do primeiro com metade do segundo é: {produto}')
+print(f'A soma do triplo do primeiro com o terceiro é: {soma}')
+print(f'O terceiro elevado ao cubo é {elevado}')
