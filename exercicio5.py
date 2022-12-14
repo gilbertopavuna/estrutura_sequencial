6ddf1d955c65ef196dedc6f718e0f4c73a64b30c
Gilberto Pavuna <gilbertopavunacosta@hotmail.com>
2022-12-14 18:22:28 -0300

    altera dados errados

--- exercicio5.py
+++ exercicio5.py
@@ -1,4 +1,4 @@
 dado = float(input('Entre com o valor em metros: '))
 cm = 100
 metro = dado*cm
-print(f'{dado} metros equivalem à: {metro:.02f} cm.')cd 
+print(f'{dado} metros equivalem à: {metro:.02f} cm.')
