e1ef861824404c9518ee4c765f524651f3f2f396
Gilberto Pavuna <gilbertopavunacosta@hotmail.com>
2022-12-14 19:22:43 -0300

    Exercicio 87 resolvido

---
+++ exercicio8.py
@@ -0,0 +1,5 @@
+ganho_por_hora = float(input('Entre com seu ganho por hora: '))
+horas_trabalhadas_no_mes = float(input('Entre com as horas trabalhadas no mês: '))
+salario_nesse_mes = ganho_por_hora * horas_trabalhadas_no_mes
+
+print(f'Seu salário nesse mês será de: R$ {salario_nesse_mes}')
